a6734c0ffc8082023626cd738e82dfc2be2f1d81
Bruno de Moraes <bruno.moraes@opah.com.br>
2021-09-20 21:36:07 -0300

    Estrutura da page

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-function onConnection(socket){
-  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+function onConnection(socket) {
+  socket.on("drawing", (data) => socket.broadcast.emit("drawing", data));
 }
 
-io.on('connection', onConnection);
+io.on("connection", onConnection);
 
-http.listen(port, () => console.log('listening on port ' + port));
+http.listen(port, () => console.log("listening on port " + port));
