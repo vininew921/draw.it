f473a6d97a1f8228528347883e182a9a22eabd04
Vinicius Barbosa <vininew921@hotmail.com>
2021-09-25 16:27:51 -0300

    Added hot reload for frontend and backend

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const livereload = require("livereload").createServer();
+const connectLivereload = require("connect-livereload");
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
+const publicDirectory = path.join(__dirname, 'public');
+
+livereload.watch(publicDirectory);
+livereload.server.once("connection", () =>{
+  setTimeout(() =>{
+    livereload.refresh("/");
+  }, 100);
+});
+
+app.use(connectLivereload());
+
+app.use(express.static(publicDirectory));
+
+
+app.get('/', (req, res) =>{
+  res.sendFile(__dirname + '/index.html');
+});
 
 function onConnection(socket) {
   socket.on("drawing", (data) => socket.broadcast.emit("drawing", data));
